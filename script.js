301e8de94ec9b87ec76f8f00f928fa25a6ddaf7d
Puneeth K <reachout.puneeth@gmail.com>
2024-12-07 17:54:26 +0530

    Add the comment in the js file

--- script.js
+++ script.js
@@ -63,7 +63,8 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+// Initialize AOS
 AOS.init({
-    duration: 1000, // Animation duration in milliseconds
-    once: false,     // Whether animation should happen only once
+    duration: 1000,
+    once: false,
 });
